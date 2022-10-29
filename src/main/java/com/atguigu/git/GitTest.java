d1b6546cc5566a8778bb2435795debd4908a6ae1
yxt <1248487420@qq.com>
2022-10-29 15:57:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+
+/**
+ * @author yxt
+ * @create 2022-10-28 14:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
